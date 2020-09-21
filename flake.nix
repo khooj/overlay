8a4730bb7624ecad74355daf3feecadd7f876a88
Igor Gilmutdinov <bladoff@gmail.com>
2020-09-21 22:15:04 +0300

    test2 flake overlay

--- flake.nix
+++ flake.nix
@@ -2,8 +2,6 @@
   description = "Custom khooj overlay flake";
 
   outputs = { self }: {
-	overlay = final: prev: {
-		custom = import ./default.nix final prev;
-	};
+	overlay = final: prev: import ./default.nix final prev;
   };
 }
