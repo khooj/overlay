545305e2be829a72ff78942a07fcf3d12fb3a723
Igor Gilmutdinov <bladoff@gmail.com>
2020-09-22 11:12:23 +0300

    commented curlify

--- default.nix
+++ default.nix
@@ -1,9 +1,9 @@
 self: super:
 {
-	python38 = super.python38.override {
-		packageOverrides = self: super: {
-			curlify = super.callPackage ./curlify {};
-		};
-	};
+	#python38 = super.python38.override {
+		#packageOverrides = eself: esuper: {
+			#curlify = esuper.callPackage ./curlify {};
+		#};
+	#};
 	nomad_0123 = super.callPackage ./nomad {};
 }
